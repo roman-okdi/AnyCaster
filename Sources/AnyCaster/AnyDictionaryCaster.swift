8b88abcef075978d172421abead7265843ac87b8
Roman <newmailri@gmail.com>
2024-06-14 17:17:34 +0400

    docs(AnyDictionaryCaster.swift): add docs for methods

--- Sources/AnyCaster/AnyDictionaryCaster.swift
+++ Sources/AnyCaster/AnyDictionaryCaster.swift
@@ -5,14 +5,21 @@
 //  Created by Roman Imanaliev on 28.05.2024.
 //
 
+/// AnyDictionaryCaster is a utility struct designed to perform type casting on a dictionary of wrapped values.
 public struct AnyDictionaryCaster {
     
+    /// The dictionary containing wrapped values to be cast.
     public let value: [AnyHashable: AnyCaster]
     
+    /// Initializes an AnyDictionaryCaster with the given dictionary.
+    /// - Parameter value: The dictionary containing wrapped values to be cast.
     public init(_ value: [AnyHashable : AnyCaster]) {
         self.value = value
     }
     
+    /// Initializes an AnyDictionaryCaster with the given value.
+    /// - Parameter value: The value to be cast to a dictionary.
+    /// - Throws: `AnyCasterError.failedCast` if the cast to dictionary fails.
     public init(_ value: Any) throws {
         guard let dict = value as? [AnyHashable: Any] else {
             throw AnyCasterError.failedCast
@@ -20,6 +27,10 @@ public struct AnyDictionaryCaster {
         self.value = dict.mapValues(AnyCaster.init)
     }
     
+    /// Retrieves the wrapped value for the given key.
+    /// - Parameter key: The key for which to retrieve the wrapped value.
+    /// - Throws: `AnyCasterError.missingValue` if the key is not found in the dictionary.
+    /// - Returns: The wrapped value for the given key.
     public func get(_ key: AnyHashable) throws -> AnyCaster {
         guard let result = value[key] else {
             throw AnyCasterError.missingValue
@@ -27,6 +38,9 @@ public struct AnyDictionaryCaster {
         return result
     }
     
+    /// Subscript for accessing wrapped values in the dictionary.
+    /// - Parameter key: The key for which to retrieve the wrapped value.
+    /// - Returns: The wrapped value for the given key, or nil if the key is not found.
     public subscript(key: AnyHashable) -> AnyCaster? {
         value[key]
     }
